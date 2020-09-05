e2ad781024a7dfed9edef7a94a641eb10e56efff
bradylangdale <bradylangdale@gmail.com>
2020-09-05 00:35:57 -0500

    Initial Upload

---
+++ Matrix.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class Matrix {
+
+    private float[,] matrix;
+
+    public int columns { get; set; }
+    public int rows { get; set; }
+
+    public Matrix(int rows, int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        matrix = new float[rows, columns];
+    }
+
+    public Matrix(int rows, int columns, bool randData)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        matrix = new float[rows, columns];
+
+        if (randData)
+        {
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    matrix[row, column] = Random.Range(-1f, 1f);
+                }
+            }
+        }
+    }
+
+    public Matrix(int rows, int columns, float[] data)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        matrix = new float[rows, columns];
+        ApplyData(data);
+    }
+
+    public Matrix(float[,] data)
+    {
+        rows = data.GetLength(0);
+        columns = data.GetLength(1);
+        matrix = data;
+    }
+
+    public float this[int row, int column]
+    {
+        get { return matrix[row, column]; }
+        set { matrix[row, column] = value; }
+    }
+
+    private void ApplyData(float[] data)
+    {
+        if (data.Length != (rows * columns)) throw new Exception("The given data array is not compatible with the matrix.");
+
+        int i = 0;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                matrix[row, column] = data[i];
+                i++;
+            }
+        }
+    }
+
+    public float[] ToArray()
+    {
+        float[] array = new float[rows * columns];
+        int i = 0;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                array[i] = matrix[row, column];
+                i++;
+            }
+        }
+
+        return array;
+    }
+
+    public static Matrix operator +(Matrix a, Matrix b) 
+    {
+        if (a.rows != b.rows || a.columns != b.columns) throw new Exception("Can not add matrices of different sizes.");
+
+        for (int row = 0; row < a.rows; row++)
+        {
+            for (int column = 0; column < a.columns; column++)
+            {
+                a[row, column] += b[row, column];
+            }
+        }
+
+        return a;
+    }
+
+    public static Matrix operator -(Matrix a, Matrix b)
+    {
+        if (a.rows != b.rows || a.columns != b.columns) throw new System.Exception("Can not subtract matrices of different sizes.");
+
+        for (int row = 0; row < a.rows; row++)
+        {
+            for (int column = 0; column < a.columns; column++)
+            {
+                a[row, column] -= b[row, column];
+            }
+        }
+
+        return a;
+    }
+
+    public static Matrix operator *(int a, Matrix b)
+    {
+        for (int row = 0; row < b.rows; row++)
+        {
+            for (int column = 0; column < b.columns; column++)
+            {
+                b[row, column] *= a;
+            }
+        }
+
+        return b;
+    }
+
+    public static Matrix operator *(float a, Matrix b)
+    {
+        for (int row = 0; row < b.rows; row++)
+        {
+            for (int column = 0; column < b.columns; column++)
+            {
+                b[row, column] *= a;
+            }
+        }
+
+        return b;
+    }
+
+    public static Matrix operator *(Matrix a, Matrix b)
+    {
+        if (a.columns != b.rows) throw new Exception("Can not multiply matrices of incompatible sizes.");
+
+        Matrix c = new Matrix(a.rows, b.columns);
+
+        for (int row = 0; row < c.rows; row++)
+        {
+            for (int column = 0; column < c.columns; column++)
+            {
+                float dot = 0;
+                for (int num = 0; num < a.columns; num++)
+                {
+                    dot += a[row, num] * b[num, column];
+                }
+                c[row, column] = dot;
+            }
+        }
+
+        return c;
+    }
+
+    public override string ToString()
+    {
+        string result = "\n";
+
+        for (int row = 0; row < rows; row++)
+        {
+            result += "[";
+            for (int column = 0; column < columns; column++)
+            {
+                result += matrix[row, column] + ((column != columns - 1) ? "," : "");
+            }
+            result += "]\n";
+        }
+
+        return result;
+    }
+}
---
+++ NeuralNetwork.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+[Serializable]
+public class NeuralNetwork {
+
+    private Matrix[] layers;
+    private Matrix[] bias;
+    private Matrix[] weights;
+
+    private Matrix[] dweights;
+    private Matrix[] dbias;
+
+    public NeuralNetwork(int input, int output, int[] hidden)
+    {
+        layers = new Matrix[hidden.Length + 2];
+        bias = new Matrix[hidden.Length + 1];
+        weights = new Matrix[hidden.Length + 1];
+
+        dweights = new Matrix[weights.Length];
+        dbias = new Matrix[bias.Length];
+
+        layers[0] = new Matrix(input, 1);
+
+        for (int i = 0; i < hidden.Length; i++)
+        {
+            layers[i + 1] = new Matrix(hidden[i], 1);
+        }
+
+        layers[hidden.Length + 1] = new Matrix(output, 1);
+
+        for (int i = 1; i < layers.Length; i++)
+        {
+            bias[i - 1] = new Matrix(layers[i].rows, 1, true);
+            weights[i - 1] = new Matrix(layers[i].rows, layers[i - 1].rows, true);
+            dbias[i - 1] = new Matrix(layers[i].rows, 1);
+            dweights[i - 1] = new Matrix(layers[i].rows, layers[i - 1].rows);
+        }
+    }
+
+    public void SaveNetwork(string path)
+    {
+        FileStream fs = new FileStream(path, FileMode.Create);
+
+        BinaryFormatter bf = new BinaryFormatter();
+
+        bf.Serialize(fs, this);
+        fs.Close();
+    }
+
+    public void LoadNetwork(string path)
+    {
+        try
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            NeuralNetwork loaded = (NeuralNetwork)bf.Deserialize(fs);
+
+            ApplyLoadedWeights(loaded.GetWeights());
+            ApplyLoadedBias(loaded.GetBias());
+            fs.Close();
+        } catch 
+        {
+            FileStream fs = new FileStream(path, FileMode.Create);
+            fs.Close();
+        } 
+    }
+
+    public Matrix[] GetWeights() { return weights; }
+
+    public Matrix[] GetBias() { return bias; }
+
+    private void ApplyLoadedWeights(Matrix[] weights) { this.weights = weights; }
+
+    private void ApplyLoadedBias(Matrix[] bias) { this.bias = bias; }
+
+    public Matrix ProcessInput(float[] input)
+    {
+        layers[0] = new Matrix(layers[0].rows, layers[0].columns, input);
+
+        for (int layer = 1; layer < layers.Length; layer++)
+        {
+            layers[layer] = Sigmoid((weights[layer - 1] * layers[layer - 1]) + bias[layer - 1]);
+        }
+
+        return layers[layers.Length - 1];
+    }
+
+    private Matrix Sigmoid(Matrix matrix)
+    {
+        for (int row = 0; row < matrix.rows; row++)
+        {
+            matrix[row, 0] = (float)(1.0 / (1.0 + Math.Pow(Math.E, -matrix[row, 0])));
+        }
+
+        return matrix;
+    }
+
+    private Matrix dSigmoid(Matrix matrix)
+    {
+        for (int row = 0; row < matrix.rows; row++)
+        {
+            matrix[row, 0] = (float)(1.0 / (4.0 * (Math.Pow(Math.Cosh(matrix[row, 0]), 2))));
+        }
+
+        return matrix;
+    }
+
+    public void ProcessOutput(Matrix dCost)
+    {
+        Matrix dZ = dSigmoid(weights[weights.Length - 1] * layers[layers.Length - 2] + bias[bias.Length - 1]);
+        weights[weights.Length - 1] += ComputeWeights(layers[layers.Length - 2], layers[layers.Length - 1], dZ, dCost);
+        bias[bias.Length - 1] += ComputeBias(layers[layers.Length - 1], dZ, dCost);
+        Matrix dcost = ComputeCost(weights[weights.Length - 1], layers[layers.Length - 2], layers[layers.Length - 1], dZ, dCost);
+
+        for (int i = 2; i < weights.Length; i++)
+        {
+            Matrix dz = dSigmoid(weights[weights.Length - i] * layers[layers.Length - (i + 1)] + bias[bias.Length - i]);
+            weights[weights.Length - i] += ComputeWeights(layers[layers.Length - (i + 1)], layers[layers.Length - i], dz, dcost);
+            bias[bias.Length - i] += ComputeBias(layers[layers.Length - i], dz, dcost);
+            dcost = ComputeCost(weights[weights.Length - i], layers[layers.Length - (i + 1)], layers[layers.Length - i], dz, dcost);
+        }
+    }
+
+    public void UpdateNetwork()
+    {
+        for (int i = 0; i < weights.Length; i++)
+        {
+            weights[i] += dweights[i];
+            bias[i] += dbias[i];
+            dweights[i] = 0 * dweights[i];
+            dbias[i] = 0 * dbias[i];
+        }
+        
+    }
+
+    private Matrix ComputeWeights(Matrix layerA, Matrix layerB, Matrix dz, Matrix dcost)
+    {
+        Matrix dweights = new Matrix(layerB.rows, layerA.rows);
+
+        for (int rowA = 0; rowA < layerA.rows; rowA++)
+        {
+            for (int rowB = 0; rowB < layerB.rows; rowB++)
+            {
+                dweights[rowB, rowA] = layerA[rowA, 0] * dz[rowB, 0] * dcost[rowB, 0]; 
+            }
+        }
+
+        return dweights;
+    }
+
+    private Matrix ComputeBias(Matrix layerA, Matrix dz, Matrix dcost)
+    {
+        Matrix dbias = new Matrix(layerA.rows, 1);
+
+        for (int rowA = 0; rowA < layerA.rows; rowA++)
+        {
+            dbias[rowA, 0] = dz[rowA, 0] * dcost[rowA, 0];
+        }
+
+        return dbias;
+    }
+
+    private Matrix ComputeCost(Matrix weight, Matrix layerA, Matrix layerB, Matrix dz, Matrix dcost)
+    {
+        Matrix dCost = new Matrix(layerA.rows, 1);
+
+        for (int rowA = 0; rowA < layerA.rows; rowA++)
+        {
+            for (int rowB = 0; rowB < layerB.rows; rowB++)
+            {
+                dCost[rowA, 0] = ((weight[rowB, 0] * dz[rowB, 0] * dcost[rowB, 0]) - layerA[rowA, 0]);
+            }
+        }
+
+        return dCost;
+    }
+
+    public override string ToString()
+    {
+        string net = "Network:\n";
+
+        net += "\tLayers:\n";
+        foreach (Matrix layer in layers)
+        {
+            net += "\t\t" + layer.rows + "," + layer.columns + "\n";
+        }
+
+        net += "\tBias:\n";
+        foreach (Matrix bia in bias)
+        {
+            net += "\t\t" + bia.rows + "," + bia.columns + "\n";
+        }
+
+        net += "\tWeights:\n";
+        foreach (Matrix weight in weights)
+        {
+            net += "\t\t" + weight.rows + "," + weight.columns + "\n";
+        }
+
+        return net;
+    }
+}
